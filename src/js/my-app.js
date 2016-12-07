d9b3bb4703ad6df1e134d98b0219f02dd91261c8
sanyang <871757525@qq.com>
2016-12-07 23:35:21 +0800

    app的基本布局

---
+++ src/js/my-app.js
@@ -0,0 +1,14 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add views
+var view1 = myApp.addView('#view-1');
+var view2 = myApp.addView('#view-2', {
+    dynamicNavbar: true
+});
+var view3 = myApp.addView('#view-3');
+var view4 = myApp.addView('#view-4');
+
