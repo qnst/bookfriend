879ac8008be65b83f894bfe5b7757b2ace29eaa5
sanyang <871757525@qq.com>
2016-12-30 11:15:49 +0800

    删除了一个目录

--- backend/book/urls.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-from django.conf.urls import url,patterns
-from django.views.generic import TemplateView
-from views import *
-
-urlpatterns=patterns(
-    "",
-)
-
--- backend/restfulapi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url, include
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'articles', views.ArticleViewSet)
