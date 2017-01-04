59e8d3496504f47c14c1ea6ae22466e422b87103
wangsanyang <wangsanyang@xiangcloud.com.cn>
2017-01-04 18:39:13 +0800

    简单的api设计

--- backend/restfulapi/serializers.py
+++ backend/restfulapi/serializers.py
@@ -1,16 +1,54 @@
+# coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+import json
+
 from rest_framework import serializers
 from book.models import Category, Book
+
+
+class MyCustField(serializers.CharField):
+    """为 Model 中的自定义域额外写的自定义 Serializer Field"""
+
+    def to_representation(self, obj):
+        """将从 Model 取出的数据 parse 给 Api"""
+        return obj
+
+    def to_internal_value(self, data):
+        """将客户端传来的 json 数据 parse 给 Model"""
+        return json.loads(data.encode('utf-8'))
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name',)
+        fields = ('id', 'name', 'c_books')
 
+        # 这句话的作用是为 MyModel 中的外键建立超链接，依赖于 urls 中的 name 参数
+        # 不想要这个功能的话完全可以注释掉
+        c_books = serializers.HyperlinkedRelatedField(
+            many=True, queryset=Book.objects.all(),
+            view_name='model-detail'
+        )
 
 class BookSerializer(serializers.ModelSerializer):
     # category = CategorySerializer()
 
     class Meta:
         model = Book
-        fields = ('title', 'cover', 'description', 'douban_score', 'users', 'tags')
+        fields = ('id', 'title', 'cover', 'description', 'douban_score', 'users', 'tags')
+        # read_only_fields = ('title',)
+
+
+    def create(self, validated_data):
+        """响应 POST 请求"""
+        # 自动为用户提交的 model 添加 owner
+        validated_data['owner'] = self.context['request'].user
+        return Book.objects.create(**validated_data)
+
+
+    def update(self, instance, validated_data):
+        """响应 PUT 请求"""
+        instance.field = validated_data.get('field', instance.field)
+        instance.save()
+        return instance
--- backend/restfulapi/views.py
+++ backend/restfulapi/views.py
@@ -1,15 +1,41 @@
 # coding: utf-8
+from __future__ import unicode_literals, absolute_import
 
 import django_filters
-from rest_framework import viewsets, filters
+from rest_framework import permissions, viewsets, filters, renderers
+from rest_framework.response import Response
+from rest_framework.decorators import (
+    permission_classes, detail_route
+)
 
+from .serializers import CategorySerializer, BookSerializer
 from book.models import Category, Book
-from serializers import CategorySerializer, BookSerializer
 
+# 参考：　https://blog.laisky.com/p/django-rest/
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        pass
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def list(self, request, *args, **kwargs):
+        return super(CategoryViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -17,3 +43,22 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
     filter_fields = ('title', 'ISBN')
+
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('title', 'ISBN', 'created_at')  # http://example.com/api/users?ordering=title,-created_at
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title', 'description')  # http://example.com/api/users?search=title
+
+    # def filter_queryset(self, request, queryset, view):
+    #     nodename = request.QUERY_PARAMS.get('title')
+    #     if nodename:
+    #         return queryset.filter(nodename=nodename)
+    #     else:
+    #         return queryset
+
+    # @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    # def plaintext(self, request, *args, **kwargs):
+    #     """自定义 Api 方法"""
+    #     model = self.get_object()
+    #     return Response(repr(model))
